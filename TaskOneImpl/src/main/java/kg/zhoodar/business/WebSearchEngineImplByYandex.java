1b789de792c10056aa18b0ced4d6b7bf805ea62b
ZHoodar <ajoodar@gmail.com>
2016-10-01 17:03:24 +0600

    Initial commit

---
+++ TaskOneImpl/src/main/java/kg/zhoodar/business/WebSearchEngineImplByYandex.java
@@ -0,0 +1,17 @@
+package kg.zhoodar.business;
+
+import kg.zhoodar.base.WebSearchEngine;
+
+import java.util.List;
+
+/**
+ * @author Zhoodar
+ * @since 01.10.2016.
+ */
+public class WebSearchEngineImplByYandex implements WebSearchEngine {
+
+    @Override
+    public List doSearch(String query) {
+        return null;
+    }
+}
---
+++ TaskOneImpl/src/main/java/kg/zhoodar/main/Main.java
@@ -0,0 +1,12 @@
+package kg.zhoodar.main;
+
+/**
+ * @author Zhoodar
+ * @since 01.10.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
